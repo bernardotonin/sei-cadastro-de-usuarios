10006785cdaa5d060ca69ba78f84e2fca4bd5d2c
bernardotonin <btprates2004@gmail.com>
2024-02-24 15:52:57 -0300

    Refatorado o codigio do arquivo util.py

--- util.py
+++ util.py
@@ -1,22 +1,29 @@
 #encoding: utf-8
+from config import SPEED
+from typing import Tuple
+import pyautogui
 import csv
-import json
-
-class Usuario:
-    def __init__(self, nome : str, email : str, sigla: str, cpf : str):
-        self.nome = nome
+class User:
+    def __init__(self, name : str, email : str, login: str, cpf : str):
+        self.name = name
         self.email = email
-        self.sigla = sigla
+        self.login = login
         self.cpf = cpf
 
 
+def searchErrorMsg() -> bool:
+    try: 
+        pyautogui.locateOnScreen('errormsg3.png')
+        return True
+    except pyautogui.ImageNotFoundException:
+        return False
 
-def carregarDados(arquivo : str):
-    with open(arquivo, newline='', encoding="utf8") as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-        resultado : list[Usuario] = []
-        for row in spamreader:
-            x = Usuario(row[0], row[1], row[2], row[3])
-            resultado.append(x)
-    return resultado     
+def loadData(file : str):
+    with open(file, newline='', encoding="utf8") as dataFile:
+        data = csv.reader(dataFile, delimiter=',')
+        userList : list[User] = []
+        for row in data:
+            x = User(row[0].title(), row[1], row[2], row[3])
+            userList.append(x)
+    return userList     
                 
